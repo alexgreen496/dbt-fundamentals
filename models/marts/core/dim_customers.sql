a570a6167e7b4e05c2fa2f8d27a65bd7fb0c850e
Alex Green <alexgreen496@gmail.com>
2022-02-13 12:17:35 +0000

    practice

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -7,6 +7,10 @@ orders as (
     select * from {{ ref('stg_orders') }}
 ),
 
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
 customer_orders as (
 
     select
@@ -14,9 +18,12 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(payments.amount)     as lifetime_value
 
     from orders
+
+    left join payments using (order_id)
 
     group by 1
 
@@ -31,6 +38,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
+        customer_orders.lifetime_value,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers
