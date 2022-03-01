d9c6aa3c63d118f9421e803a65635d2b4c562cdc
Alex Green <alexgreen496@gmail.com>
2022-03-01 18:29:29 +0000

    added cents to dollars macro and utils package

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,14 +1,17 @@
 with payments as (
 
     select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status,
-        amount/100 as amount,
-        created as created_at
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 4) }} as amount,
+    created as created_at
 
-    from {{ source('stripe', 'payment') }}
+    from {{ source('stripe','payment') }}
+
+
 
 )
 
